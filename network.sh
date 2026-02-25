6812b55b864c9a8b67e65c85afd353ad45f1fdb1
Klurus <162215169+Klurus@users.noreply.github.com>
2026-02-25 09:02:18 +0300

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-hostnamectl set-hostname idc-dc.au.team; exec bash
+hostnamectl set-hostname idc-dc.au.team
 echo "192.168.100.10/24" > /etc/net/ifaces/ens19/ipv4address
 echo "default via 192.168.100.1" > /etc/net/ifaces/ens19/ipv4route 
 echo "search au.team" > /etc/net/ifaces/ens19/resolv.conf
 echo "nameserver 192.168.100.1" > /etc/net/ifaces/ens19/resolv.conf 
 systemctl restart network
+exec bash
